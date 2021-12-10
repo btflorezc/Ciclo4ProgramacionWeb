061c198d8fedc3271a3df6421f9f4ec34cf7e912
btflorezc <btflorezc@gmail.com>
2021-12-09 22:34:15 -0500

    Se adiciona project-management (frontend).

---
+++ project-management/src/controllers/users.controller.js
@@ -0,0 +1,22 @@
+const usersCtrl = {};
+usersCtrl.renderSignUpForm = (req, res) => {
+    res.render('users/signup');
+};
+
+usersCtrl.signup = (req, res) => {
+    res.send('Registrarme');
+};
+
+usersCtrl.renderSignInForm = (req, res) => {
+    res.render('users/signin');
+};
+
+usersCtrl.signin = (req, res) => {
+    res.send('Iniciar sesión');
+};
+
+usersCtrl.logout = (req, res) => {
+    res.send('Cerrar sesión');
+};
+
+module.exports = usersCtrl;
---
+++ project-management/src/views/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const exphbs = require('express-handlebars')
+const path = require('path');
+
+// Inicializaciones
+const app = express();
+
+// Settings (Ajustes)
+app.set('port', process.env.PORT || 4000);  
+app.set('views', path.join(__dirname, '../../src/views'));
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: "main",
+    layoutsDir: '../../src/views/layouts',
+    partialsDir: '../../src/views/partials',
+    extname: '.hbs'
+}));
+app.engine('hbs', require('exphbs'));
+app.set('view engine', 'hbs');
+
+
+// Middlewares
+app.use(express.urlencoded({extended: false}));
+
+// Global Variables
+
+// Routes
+app.get('/', (req, res) =>{
+    res.render('index.hbs');
+});
+app.use(require('../routes/users.routes'));
+
+// Static files
+//app.use(express.static(path.join(__dirname,'../../public')));
+
+module.exports = app;
